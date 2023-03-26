696e270afe9bca5ea7a50e8e44a051dcba7b0721
navish21 <128972075+navish21@users.noreply.github.com>
2023-03-26 17:34:49 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,42 @@
-## Pair of functions that cache the inverse of a matrix
+## Pair of fucntions that cache the inverse of matrix
+## Usage: Pass the result of a makeCacheMatrix call too cacheSolve
 
-## Usage: Pass the result of a makeCacheMatrix call to cacheSolve 
 #' Util function that set the matrix and the inverse in an environment
-
-#' x = makeCacheMatrix(matrix(rnorm(9), 3, 3))
-
-#' x$set(matrix(rnorm(16), 4, 4))
+#' @param x an invertible matrix
+#' examples
+#' c = makeCacheMatrix(matric(rnorm(9), 3, 3))
+#' x$set(matrix(rnorm(16), 4, 4)) 
 makeCacheMatrix <- function(x = matrix()) {
-  # todo error if x is not a matrix
+  #todo error if x is not a matrix
   inv <- NULL
   set <- function(y) {
-    x <<- y
+    x <<-y
     inv <<- NULL
-  }
+    }
   get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
+  setinvrse <- function(inverse) inv <<-inverse
   getinverse <- function() inv
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
-}
+  }
 
 
-#' Compute and cache the inverse of a matrix
+#' Compute and cahe the inverse of a matrix
 #' @param x the result of a previous makeCacheMatrix call
 #' @param ... additional arguments to pass to solve function
 #' examples
 #' x = makeCacheMatrix(matrix(rnorm(9), 3, 3))
-#' cacheSolve(x)
+#' cachSolve(x)
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  ##Return a matrix that is the inverse of 'x'
   inv <- x$getinverse()
   if(!is.null(inv)) {
     message("getting cached matrix inverse")
     return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data, ...)
+    }
+  data <= x$get()
+  inv - solve(data, ...)
   x$setinverse(inv)
   inv
-}
+  }
